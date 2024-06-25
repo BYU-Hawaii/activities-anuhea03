ad9ace3032b5d608eb875bb9414486346cb0eafc
Anuhea <anuhea03@go.byuh.edu>
2024-06-24 18:30:51 -1000

    changes

--- Final/scripts/scripts.js
+++ Final/scripts/scripts.js
@@ -1,16 +1,8 @@
 function myFunction() {
     var x = document.getElementById("menu-bar");
     if (x.className === "menu-bar") {
-        x.className += "responsive";
+        x.className += " responsive";
     } else {
         x.className = "menu-bar";
     }
 }
-
-// Event listener for window resize to handle responsive behavior
-window.onresize = function() {
-    var x = document.getElementById("menu-bar");
-    if (window.innerWidth > 768) {
-        x.className = "menu-bar";
-    }
-};
