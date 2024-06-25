3db4f4e6be9a0fae1f59b39ead93f531f88e9582
Anuhea <anuhea03@go.byuh.edu>
2024-06-24 23:06:07 -1000

    changes

--- Final/scripts/scripts.js
+++ Final/scripts/scripts.js
@@ -1,8 +1,21 @@
-function myFunction() {
-    var x = document.getElementById("menu-bar");
-    if (x.className === "menu-bar") {
-        x.className += " responsive";
+function validateForm() {
+    var name = document.forms["eventForm"]["name"].value;
+    var address = document.forms["eventForm"]["address"].value;
+    var guests = document.forms["eventForm"]["guests"].value;
+
+    if (name == "" || address == "" || guests == "") {
+        alert("All fields must be filled out.");
+        return false;
     } else {
-        x.className = "menu-bar";
+        // Prevent the default form submission
+        event.preventDefault();
+        displayThankYou();
+        return false;
     }
 }
+
+function displayThankYou() {
+    var formElement = document.getElementById('eventForm');
+    formElement.innerHTML = '<h3>Thank you for registering!</h3><p>We look forward to seeing you at the event.</p>';
+}
+
